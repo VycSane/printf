11ab11b68d4dd8f994475f2c7132c6e7e0cf04cf
Julieteiluj <ndukwejuliet41@gmail.com>
2022-08-12 08:49:38 -0700

    get_flag.c

--- get_flag.c
+++ get_flag.c
@@ -4,9 +4,10 @@
  * get_flag - turns on flags if _printf finds
  * @s: character that holds the flag specifier
  * @f: pointer to the struct flags in which we turn the flags on
- * Return: 1 if flag has been turned on, 0 otherwise
+ *
+ * Return: 1 if a flag has been turned on, 0 otherwise
  */
-int get_flag(char s, flag *f)
+int get_flag(char s, flags_t *f)
 {
 	int i = 0;
 	switch (s)
